639160a3a4ccdde309e1a32ba42ad7348c342a6a
kendy92 <dinhconganh@gmail.com>
2018-06-11 22:19:46 -0400

    add car component, model and mock data, integrate boostrap framework

---
+++ src/app/cars/car.ts
@@ -0,0 +1,14 @@
+export class Car {
+    id: number; 
+    type: string;
+    year: number;
+    make: string;
+    model: string;
+    transmissionType: string;
+    vehicleStatus: string;
+    price: number;
+    fuelType: string;
+    kilometres: number;
+    hasCarProof: boolean;
+    datePosted: Date;
+}
