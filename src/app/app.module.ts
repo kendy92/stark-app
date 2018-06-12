4823bee3eba2def25b82f219e77a017f22d248ac
aqsmalhotra <n01249822@humbermail.ca>
2018-06-12 09:34:04 -0400

    add dummy data to client list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { CustomersComponent } from './customers/customers.component';
   declarations: [
     AppComponent,
     CarsComponent,
-    CustomersComponent
+    CustomersComponent,
   ],
   imports: [
     BrowserModule,
